c35cc273b32d5f67a2bb3c7ac2fc1ff120c59a66
Rodrigo Lins <rlinsdev@gmail.com>
2021-03-15 13:51:24 -0300

    navbar and routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from "react"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
-    <div>
-      <h2>Design</h2>
-      <h3>Develop & Deploy</h3>
-      <p>UX Designer & web developer based</p>
-    </div>
+    <section>
+      <Navbar />
+      <div>
+        <h2>Design</h2>
+        <h3>Develop & Deploy</h3>
+        <p>UX Designer & web developer based</p>
+      </div>
+    </section>
   )
 }
