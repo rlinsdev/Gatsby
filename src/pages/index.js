ed3cd79970fe6957299b9beb84076be68f2ee3bd
Rodrigo Lins <rlinsdev@gmail.com>
2021-03-15 15:54:37 -0300

    Styling components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
 import React from "react"
+import { Link } from 'gatsby'
 import Layout from "../components/Layout"
+// import styles from '../styles/home.module.css'
+import * as styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
-          <p>UX Designer & web developer based</p>
+          <p>UX designer & web developer based in Manchester.</p>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
       </section>
     </Layout>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import * as styles from '../../styles/projects.module.css'
 
 export default function Project() {
   return (
     <Layout>
-      <div>
+      <div className={styles.portfolio}>
         <h1>Index page of path Project</h1>
       </div>
     </Layout>
