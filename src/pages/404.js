0e8886f0f4cb75faec2f0228329656add8848e38
Rodrigo Lins <rlinsdev@gmail.com>
2021-03-15 14:32:12 -0300

    Layout component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Oops!</h1>
-    </div>
+    <Layout>
+      <div>
+        <h1>Oops!</h1>
+      </div>
+    </Layout>
   )
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,17 @@
 //rfc - React Functional Component
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function About() {
   return(
-    <div>
-      <h1>About</h1>
-      <p>1</p>
-      <p>2</p>
-      <p>3</p>
-      <p>4</p>
-    </div>
+    <Layout>
+      <div>
+        <h1>About</h1>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+      </div>
+    </Layout>
   )
 }
